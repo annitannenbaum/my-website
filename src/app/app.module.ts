15787a2b314e4819f0652da10f12f516579c3c25
Anni Hoege <annahoege@gmx.de>
2021-07-17 18:32:55 +0200

    Added routing, some refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { registerLocaleData } from '@angular/common';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
@@ -9,13 +8,13 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { MyDogComponent } from './components/my-dog/my-dog.component';
-import { RamenComponent } from './components/ramen/ramen.component';
 import { PhotographyComponent } from './components/photography/photography.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { LocaleComponent } from './shared/locale/locale.component';
 import { PageLayoutComponent } from './shared/page-layout/page-layout.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { TechComponent } from './components/tech/tech.component';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
@@ -26,13 +25,13 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
   declarations: [
     AppComponent,
     ProjectsComponent,
-    MyDogComponent,
-    RamenComponent,
     PhotographyComponent,
     MenuComponent,
     LocaleComponent,
     PageLayoutComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    AboutComponent,
+    TechComponent
   ],
   imports: [
     BrowserModule,
